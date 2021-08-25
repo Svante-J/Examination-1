0069473c82a89ab94b093539a10c81c6b5dd671f
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-08-25 22:14:18 +0200

    carousel klar (gillar inte ändrad storlek)
    klar men får lite error om man ändrar skärmstorlek

--- carousel.js
+++ carousel.js
@@ -4,7 +4,6 @@ const nextButton = document.querySelector('.carousel__button--right');
 const prevButton = document.querySelector('.carousel__button--left');
 const dotsNav = document.querySelector('.carousel__nav');
 const dots = Array.from(dotsNav.children);
-
 const slideWidth = slides[0].getBoundingClientRect().width;
 //console.log(slideWidth); 
 
@@ -16,33 +15,69 @@ slides.forEach(setSlidePosition);
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-   currentSlide.classList.remove('current-slide');
-   targetSlide.classList.add('current-slide');
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length - 1 ) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    }  else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
 }
 
 // when i click left, move slides to the left
 prevButton.addEventListener('click' , e => {
-const currentSlide = track.querySelector('.current-slide');
-const prevSlide = currentSlide.previousElementSibling; 
-moveToSlide(track, currentSlide, prevSlide);
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+
 });
 //when i click right move slide to th right
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
-   const nextSlide = currentSlide.nextElementSibling;
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
     
-   moveToSlide(track, currentSlide, nextSlide);
 });
 
 // When i click the nav indicators, move to thet slide
-dotsNav.addEventListener('click', e =>{
+dotsNav.addEventListener('click', e => {
     // what indicator was clicked on??
     const targetDot = e.target.closest('button');
     
     if(!targetDot) return;    
-    const currentSlide = track.querrySelector('.current-slide');
+
+    const currentSlide = track.querySelector('.current-slide');
     const currentDot = dotsNav.querySelector('.current-slide');
     const targetIndex = dots.findIndex(dot => dot === targetDot);
-    console.log(targetIndex);
+    const targetSlide = slides[targetIndex];
 
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex); 
+    
 });
